30dfdb6818c266d2072f5fe48081e50875058572
Saran092 <2k21ece092@kiot.ac.in>
2025-03-02 21:14:48 +0530

    Basic LinkedList

---
+++ List/ListLink.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ListLink {
+
+    public static void main(String[] args) {
+
+        List <String> linked = new LinkedList<>();
+        linked.add("Saran");
+        linked.add("Learning");
+        linked.add("Collections");
+        linked.add("In Java");
+
+        System.out.println(linked);
+
+    }
+}
